66e8ab5396f9a4c864b50f265a664528779542f1
Badr Fenni <salutbadr47@gmail.com>
2026-09-25 11:52:09 +0000

    User service with dao methodes

--- src/main/java/ma/youcode/lineperm/service/UserService.java
+++ src/main/java/ma/youcode/lineperm/service/UserService.java
@@ -8,6 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import ma.youcode.lineperm.dao.UserDao;
 
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -17,36 +18,14 @@ import ma.youcode.lineperm.model.FichierProtege;
 
 public class UserService {
 
-    private HashMap<String, User> comptes = new HashMap<>();
+    private UserDao userDao;
 
     public UserService() {
-        loadUsers();
-    }
-
-    private void loadUsers() {
-        File file = new File("users.txt");
-        if (!file.exists()) {
-            return;
-        }
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(":");
-                if (parts.length == 2) {
-                    String username = parts[0];
-                    String passHash = parts[1];
-                    User user = new User(username, passHash);
-                    comptes.put(username, user);
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading file");
-        }
+        userDao = new UserDao();
     }
 
     public boolean exists(String username) {
-        return comptes.containsKey(username);
+        return userDao.findByUsername(username) != null;
     }
 
     public boolean createAccount(String username, String passHach) {
@@ -55,44 +34,33 @@ public class UserService {
         } else {
             String passHash = BCrypt.hashpw(passHach, BCrypt.gensalt());
             User user = new User(username, passHash);
-            comptes.put(username, user);
-            saveUser(username, passHash);
+            userDao.save(user);
             return true;
         }
     }
 
     public boolean authenticate(String username, String passHach) {
-        if (!exists(username)) {
+        User user = userDao.findByUsername(username);
+
+        if (user == null) {
             return false;
-        } else {
-            User user = comptes.get(username);
-            if (BCrypt.checkpw(passHach, user.getPass())) {
-                return true;
-            } else {
-                return false;
-            }
         }
+
+        return BCrypt.checkpw(passHach, user.getPass());
     }
 
-    public boolean saveUser(String username, String passHach) {
-        try (FileWriter fw = new FileWriter("users.txt", true)) {
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw);
-            out.println(username + ":" + passHach);
-            out.flush();
-            return true;
-        } catch (IOException e) {
-            return false;
-        }
-    }
+
+    
 
     public static String ownerCurrentUser() {
-        String currentUser = System.getProperty("user.name");
-        return currentUser;
+    String currentUser = System.getProperty("user.name");
+    return currentUser;
     }
 
-    public static boolean checkPermission(String fileOwner, String currentUser, String permissions, char action) {
-        FichierProtege fichierProtege = new FichierProtege("", fileOwner, permissions);
-        return ControleAcces.estAutorise(currentUser, fichierProtege, action);
-    }
+    // public static boolean checkPermission(String fileOwner, String currentUser,
+    // String permissions, char action) {
+    // FichierProtege fichierProtege = new FichierProtege("", fileOwner,
+    // permissions);
+    // return ControleAcces.estAutorise(currentUser, fichierProtege, action);
+    // }
 }
